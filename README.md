85f078cef2dcfe2e2c9f470d4e026c001ed30ad4
mojtaba eshghie <mojtaba.eshghi@ut.ac.ir>
2020-08-07 09:45:03 +0000

    changed startup.py file to make it more beautiful, and changed the readme file

--- README.md
+++ README.md
@@ -9,13 +9,20 @@
 ![VPP Deployment](https://raw.githubusercontent.com/mojtaba-eshghie/EdgeVPP/master/src/vpp-edge-deployment.PNG)
 
 ### Usage
-You must have Docker installed. Besides, add the docker to sudoers on linux. Then, issue the following commands:
+You must have Docker installed. Besides, add the docker to sudoers on linux. Then, issue the following commands (note that during the setup any stopped or dangling docker image will be deleted):
 
     python3 startup.py
 
-After successful running of the above command you can hop into the routers using following:
+After successful running of the above command you can log into the router's CLI using following:
 
-    
+    docker exec -it router_a bash
+    docker exec -it router_b bash
+
+After attaching to the router_a/router_b containers, use the `vppctl` command to log in to the VPP CLI.
+The routers can ping each others using the following command:
+
+    ping da01::2
+
 
 Feel free to open issues if you have any questions.
 By @mojtaba-eshghie
--- startup.py
+++ startup.py
@@ -16,40 +16,40 @@ DOCKER_REMOVE = 'docker rmi -f {container}'
 DOCKER_RUN = 'docker run -v /etc/vpp -d --name {container} router'
 
 
-print('------------------------- CloudVPP Demo -------------------------')
+print('\n------------------------- EdgeVPP Demo -------------------------\n')
 
 # container stopping phase:
-print('### container stopping phase')
+print('\n### container stopping phase')
 for container in CONTAINERS:
     os.system(DOCKER_STOP.format(container=container))
 os.system('docker system prune')
 
 
 # image removal phase
-print('### image removal phase')
+print('\n### image removal phase')
 for container in CONTAINERS:
     os.system(DOCKER_REMOVE.format(container=container))
 
 
 # building phase
-print('### building phase')
+print('\n### building phase')
 os.system(DOCKER_BUILD)
 
 
 # running phase
-print('### running phase')
+print('\n### running phase')
 os.system(DOCKER_RUN.format(container='router_a'))
 os.system("docker run -d --volumes-from router_a --name router_b router")
 
 
 # execution phase
-print('### execution phase')
+print('\n### execution phase')
 print('Applying configurations, this takes a while. Sit tight...')
 for container in CONTAINERS:
     config_path = 'conf/{container}.conf'.format(container=container)
     with open(config_path, 'r') as command_file:
         print(config_path)
         for vpp_command in command_file.readlines():
-            print('applying: {vpp_command}')
+            print('applying: {vpp_command}'.format(vpp_command=vpp_command))
             time.sleep(1)
             os.system(DOCKER_EXEC.format(vpp_command=vpp_command, container=container))
